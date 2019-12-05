f2bc583c37acb34666a394f194cf49c89a54ba83
taylor <taylor@baremetalcloud.com>
2019-12-04 22:26:44 -0500

    created serializers for the recipes model.

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,2 +1,24 @@
 from rest_framework import serializers
-from recipes.models import Recipe, Step, I
+from recipes.models import Recipe, Step, Ingredient
+
+
+class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+    steps = serializers.StringRelatedField(many=True)
+    ingredients = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Recipe
+        # Query recipe object
+        fields = ('owner', 'name', 'steps', 'ingredients', 'url')
+
+
+class StepSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Step
+        fields = ('id', 'step_text', 'recipe', 'url')
+
+
+class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'text', 'recipe', 'url')
