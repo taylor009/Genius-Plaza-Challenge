328902a376de68c65b946cbe7e95721f0f7a0990
taylor <taylor@baremetalcloud.com>
2019-12-04 22:29:31 -0500

    added models to admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe, Step, Ingredient
 
-# Register your models here.
+# Register Models here.
+admin.site.register(Recipe)
+admin.site.register(Step)
+admin.site.register(Ingredient)
