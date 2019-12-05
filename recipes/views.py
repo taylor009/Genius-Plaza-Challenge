fd8e4e72bad1e226905c6261caf6a24b29e78b7f
taylor <taylor@baremetalcloud.com>
2019-12-04 22:31:38 -0500

    added viewset and models to views.py.

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from recipes.models import Recipe, Step, Ingredient
+from rest_framework import viewsets
+from recipes.serializers import RecipeSerializer, StepSerializer, IngredientSerializer
 
-# Create your views here.
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class StepViewSet(viewsets.ModelViewSet):
+    queryset = Step.objects.all()
+    serializer_class = StepSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
